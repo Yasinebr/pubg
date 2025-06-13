be126a14f13a04dbf6abc45977426ef81e2ac0dd
Yasinebr <ebrahimiy07@gmail.com>
2025-06-13 12:55:31 +0330

    'new'

---
+++ Desktop/pubgm-st-react-hud/interfaces.ts
@@ -0,0 +1,14 @@
+export interface teamPoints {
+    team_id: number,
+    team_points: number,
+    team_elms: number
+};
+
+export interface teamPointsRow {
+    id: number,
+    team_id: number,
+    team_points: number,
+    team_elms: number
+};
+
+
